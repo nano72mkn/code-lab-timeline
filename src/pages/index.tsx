3bb3ccc77f8dfbf510ef309326889929f97a1332
Shota Miyahira <2525.natuta@gmail.com>
2022-04-14 18:35:43 +0900

    ts追加

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import { NextPage } from 'next';
+import NextHeadSeo from 'next-head-seo';
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <NextHeadSeo title="Home" />
+    </>
+  );
+};
+
+export default Home;
