094b5b98c6f9f5027797572cd18a62261e43f32c
Shota Miyahira <2525.natuta@gmail.com>
2022-04-15 19:42:53 +0900

    rss追加

--- src/config/sites.ts
+++ src/config/sites.ts
@@ -3,4 +3,7 @@ import type { Url } from 'type';
 export const sites: Url[] = [
   'https://zenn.dev/shota1995m/feed',
   'https://qiita.com/shota1995m/feed',
+  'https://github.com/shota1995m/code-lab-timeline/releases.atom',
+  'https://github.com/shota1995m/planning-poker-remix/releases.atom',
+  'https://github.com/shota1995m/github-readme-blog-score/releases.atom',
 ];
