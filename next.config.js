ddb66f3e56081bda308e9e0896134dafc330bc3b
Shota Miyahira <2525.natuta@gmail.com>
2022-08-21 20:10:50 +0900

    blogのリダイレクトを設定

--- next.config.js
+++ next.config.js
@@ -2,4 +2,13 @@ module.exports = {
   images: {
     domains: ['www.google.com'],
   },
+  async redirects() {
+    return [
+      {
+        source: '/blog',
+        destination: 'https://blog.code-lab.xyz',
+        permanent: false,
+      },
+    ];
+  },
 };
